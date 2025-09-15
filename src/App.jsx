6f08c82558fa6e24c8712c2d3752bfa95335b082
siyasjr <siyasjr@gmail.com>
2025-09-15 14:54:21 +0530

    Corrected ui , Added Providers and Wallet connect/ disconnect buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
 import './App.css'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { WalletModalProvider, WalletMultiButton, WalletDisconnectButton } from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+
+
 
 function App() {
   return (
+
+    <ConnectionProvider endpoint='https://api.devnet.solana.com'>
+      <WalletProvider wallets={[]}>
+        <WalletModalProvider>
+
+          <div style={{display: 'flex',
+                       justifyContent: 'space-between',
+                       padding: 20,
+                      
+          }}>
+
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+
+            
+            </div>
+
     <TokenLaunchpad></TokenLaunchpad>
+
+     </WalletModalProvider>
+      </WalletProvider>
+     </ConnectionProvider>
+
+
   )
 }
 
